02eaf57b18816c7f743884370b26c4c386937ad0
Raneeko <Yakuranika@gmail.com>
2024-11-16 21:42:56 +0800

    Создана функция загрузки ссылки на изображение

--- Dogs.py
+++ Dogs.py
@@ -5,7 +5,15 @@ from io import BytesIO
 from tkinter import messagebox as mb
 
 
-
+def get_dog_image():
+    try:
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data = response.json()
+        return data("message")
+    except Exception as err:
+        mb.showerror("Error", f"{err}")
+        return None
 
 
 def show_image():
@@ -13,10 +21,11 @@ def show_image():
     if image_url:
         try:
             response = requests.get(image_url, stream=True)
-            response.raise_for_status()  # Получаем статус ответа
+            response.raise_for_status()  # Получаем статус ответа. Оно как бы обращается к сайту и получает ошибки по типу 512 или 404
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as err:
